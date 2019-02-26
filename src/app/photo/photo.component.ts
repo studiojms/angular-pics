0518ff8888dfba82d3acaa9b6140ec68527e1525
Jefferson M Souza <jefferson.msouza86@gmail.com>
2019-02-25 22:57:46 -0300

    getting started

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pic-photo',
+  templateUrl: './photo.component.html',
+})
+export class PhotoComponent {
+  imgUrl = 'https://www.englishpracticeonline.com/wp-content/uploads/2015/03/test.jpg';
+  imgAlt = 'Test';
+}
