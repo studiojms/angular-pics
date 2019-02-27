15854396de5fd55a0041101151e49e74ae0fbb30
jefferson.souza <jefferson.souza@synchro.com.br>
2019-02-27 13:13:08 -0300

    reading props in the component

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'pic-photo',
   templateUrl: './photo.component.html',
 })
 export class PhotoComponent {
-  imgUrl = 'https://www.englishpracticeonline.com/wp-content/uploads/2015/03/test.jpg';
-  imgAlt = 'Test';
+  @Input()
+  url = '';
+
+  @Input()
+  alt = '';
 }
