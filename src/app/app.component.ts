2b2d31784c188f5d01dfefd6b18931ec2bd99159
Jefferson M Souza <jefferson.msouza86@gmail.com>
2019-02-27 22:48:25 -0300

    using modules and ngFor directive to loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  photos = [
+    {
+      url: 'https://www.aquitemplacas.com.br/img/produtos/g/36-atencao-area-de-teste.jpg',
+      description: 'Other test',
+    },
+    {
+      url: 'http://marcelotoledo.com/wp-content/uploads/2016/04/teste-ab-checklist.jpg',
+      description: 'Test',
+    },
+  ];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PhotoComponent } from './photo/photo.component';
+import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
-  declarations: [AppComponent, PhotoComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PhotosModule],
   providers: [],
   bootstrap: [AppComponent],
 })
