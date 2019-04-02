ce95074b1a2576b5b8b122076848ce11e777d66c
Jefferson M Souza <jefferson.msouza86@gmail.com>
2019-04-01 22:53:32 -0300

    wip: implementing form validation

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginPageComponent } from './login/login.page.component';
 
 @NgModule({
   declarations: [LoginPageComponent],
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class HomeModule {}
--- src/app/home/login/login.page.component.ts
+++ src/app/home/login/login.page.component.ts
@@ -1,6 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login.page.component.html',
 })
-export class LoginPageComponent {}
+export class LoginPageComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+}
