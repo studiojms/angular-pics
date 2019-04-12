8c78b24c77121a4e14681e20da918ec0bba28c56
Jefferson M Souza <jefferson.msouza86@gmail.com>
2019-04-11 21:49:07 -0300

    implementing login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { LoginPageComponent } from './login/login.page.component';
 import { ValidationMessageModule } from '../shared/validation-message/validation-message.module';
 
 @NgModule({
   declarations: [LoginPageComponent],
-  imports: [CommonModule, ReactiveFormsModule, ValidationMessageModule],
+  imports: [CommonModule, ReactiveFormsModule, ValidationMessageModule, RouterModule],
 })
 export class HomeModule {}
--- src/app/home/login/login.page.component.ts
+++ src/app/home/login/login.page.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
+
 @Component({
   templateUrl: './login.page.component.html',
 })
 export class LoginPageComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required],
     });
+  }
+
+  login() {
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService.authenticate(userName, password).subscribe(
+      () => this.router.navigate(['photos', userName]),
+      err => {
+        console.log(err);
+        this.loginForm.reset();
+      }
+    );
   }
 }
